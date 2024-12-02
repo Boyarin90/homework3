07760f1a31620fedcd84dd84abbf10f83cf70928
Boyarin90 <lomoti1990@gmail.com>
2024-12-02 10:30:45 +0900

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,11 @@
+immutable_var = (1, 'apple', 1.2)
+print(immutable_var)
+# immutable_var.remove('apple')
+# нельзя изменить его написания так как это
+# Кортеж- не изменяемый тип данных его вводят либо
+# без скобок либо в круглых скобках. Кортеж нужен для того если нужно
+# сохранить какие либо данные. Типы в кортежах можно писать любые.
+mutable_list=['pen','pineapple','apple']
+mutable_list.append('pen')
+print(mutable_list)
+
